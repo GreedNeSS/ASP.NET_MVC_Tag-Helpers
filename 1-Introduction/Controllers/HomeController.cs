c13fc28962e80b69c5a9604c959697224ef2c77a
GreedNeSS <ruslan199126@gmail.com>
2022-07-12 17:12:33 +0300

    Added into example.

---
+++ 1-Introduction/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace _1_Introduction.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Contacts()
+        {
+            return Content("Contacts Page");
+        }
+    }
+}
--- 1-Introduction/Program.cs
+++ 1-Introduction/Program.cs
@@ -1,6 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddMvc();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{Id?}");
 
 app.Run();
