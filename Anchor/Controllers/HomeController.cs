f8aa320ed318a3a2f64c9795ea4de2db921c6986
GreedNeSS <ruslan199126@gmail.com>
2022-07-12 21:24:16 +0300

    Added AnchorTagHelpers example.

---
+++ Anchor/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Anchor.Models;
+using System;
+
+namespace Anchor.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Users(int? id)
+        {
+            string result = "Users Page";
+            if (id.HasValue) result += $"\nId = {id}";
+            return Content(result);
+        }
+
+        public IActionResult Person(Person person)
+        {
+            return Json(person);
+        }
+    }
+}
