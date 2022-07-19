86c04d174a38371416ed40855b91771aff731800
GreedNeSS <ruslan199126@gmail.com>
2022-07-19 12:57:30 +0300

    Create new project

---
+++ 9-Context_and_DI/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _9_Context_and_DI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
